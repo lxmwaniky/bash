ae036a8a5f42c5df79d78d91ce9ad36b5c425214
Alex Mwaniki <lekko254@gmail.com>
2023-08-13 01:03:23 +0300

    Variables

--- bestdayever.sh
+++ bestdayever.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "Good Morning Alex"
+name="Alex"
+echo "Good Morning $name"
 sleep 1
-echo "You're looking good today Alex!!"
+echo "You're looking good today $name!!"
 sleep 1
-echo "You have the best beard I've ever seen Alex!!"
+echo "You have the best beard I've ever seen $name!!"
